aa08facb61a735ba092fef76db5f6f4926475b12
Ashutosh jha <jha.ashutosh2004@gmail.com>
2022-09-16 16:03:05 +0530

    initial3

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const product = require('./products/api');
+const cors = require('cors');
  
 const app = express();
 
 const PORT = process.env.PORT || 5050;
  
+app.use(cors({
+    methods: ['GET','POST','DELETE','UPDATE','PUT','PATCH']
+}));
 
  app.use("/products/api",product);
 
